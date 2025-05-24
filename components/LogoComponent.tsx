d6532d411272c689d2552f8277b852bd94582da4
CodeMat17 <codemat.biz@gmail.com>
2025-05-24 01:25:38 +0100

    updated

--- components/LogoComponent.tsx
+++ components/LogoComponent.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const LogoComponent = () => {
   return (
-    <div className='flex items-center gap-1.5 sm:gap-2'>
-      <div className='border-4 border-[#179BD7] rounded-full'>
-        <Image alt='' priority width={50} height={50} src='/logo.webp' />
-      </div>
+    <div>
+      <Link href='/'>
+        <div className='flex items-center gap-1.5 sm:gap-2'>
+          <div className='border-4 border-[#179BD7] rounded-full'>
+            <Image alt='' priority width={50} height={50} src='/logo.webp' />
+          </div>
 
-      <div className='leading-tight sm:leading-3 text-white'>
-        <p className='font-semibold  sm:text-lg'>Directorate of Competitions</p>
-        <p className=''>Godfrey Okoye University</p>
-      </div>
+          <div className='leading-tight sm:leading-3 text-white'>
+            <p className='font-semibold  sm:text-lg'>
+              Directorate of Competitions
+            </p>
+            <p className=''>Godfrey Okoye University</p>
+          </div>
+        </div>
+      </Link>
     </div>
   );
 };
